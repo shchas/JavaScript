7859bed5b2c219bf15e95223753c1d0cd56168fa
shchas <shchas.173@yandex.ru>
2021-11-16 17:37:08 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer);
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваша Фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+"use strict";
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`)
+
+// const user = "Ivan";
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
---
+++ js/script1.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const persone = `Alex s12312 sdkfjah;gfowewoeir sdaof[weoy sdkfjsdfsd]`;
+
+const bool = true;
+
+const.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+//console.log(obj.name)
+console.log(obj['name']);
+
+let arr = ['plum.png','orange.jpg', 5, 'apple.bmp', {},[] ];
+console.log(arr[2]);
