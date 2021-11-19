57ae37c1c6fb9fe53ae06fd68248113f44d38af8
shchas <shcahs.173@yandex.ru>
2021-11-19 17:03:52 +0900

    015 dz

--- js/012 script.js
+++ js/012 script.js
@@ -32,12 +32,12 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB);
--- js/script.js
+++ js/script.js
@@ -22,4 +22,94 @@
 
 /* Домашнее задание */
 
-const numberOfFilms = prompt("Сколько фильмов вы посмотрели?","18");
+// if(1 == 1) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
+
+// const num = 50;
+// if(num < 49){
+//     console.log('Error');
+// } else if(num >100){
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
+// const num = 56;
+
+// (num === 56) ? console.log('ok!') : console.log('Error');
+
+// switch(num){
+//     case 49:
+//         console.log('Neverno');
+//         break;
+//     case 100:
+//         console.log('Neverno');
+//         break;
+//     case 56:
+//         console.log('Vse verno!');
+//         break;
+//     default:
+//         console.log('Ne v etot raz');
+//         break;
+// }
+
+// "use strict";
+
+// let num = 50;
+// while(num <= 55){
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num < 55);
+
+// for(let i=1; i<8; i++){
+// console.log(i);
+// }
+
+// for(let i=1; i<10; i++){
+//     if(i===6){
+//         //continue;
+//         break;
+//     }
+//     console.log(i);
+// }
+
+"use strict";
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    console.log(num);
+}
+
+showFirstMessage('Hello world!');
+console.log(num);
+
+function calc(a, b) {
+    return (a + b);
+}
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function() {
+    console.log("Hello");
+};
+
+// const calc = (a, b) => a + b ;
