b6745276563b037fc49744e712f63372dd4d3688
shchas <shcahs.173@yandex.ru>
2022-03-11 16:25:04 +0900

    46

--- js/script.js
+++ js/script.js
@@ -1,25 +1,81 @@
 'use strict';
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello ${this.name}`);
-    };
-}
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
 
-User.prototype.exit = function() {
-    console.log(`Пользователь ${this.name} ушел`)
+//     console.log(sum());
+// }
+// showThis(4,5);
+
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//     }
+// };
+// obj.sum();
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('Hello! ' + this.name);
+//     };
+//     }
+//     let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+// 1) Обычная функция: this = windows, но если use strict - undefined
+// 2) Контекст у методов объекта - сам объект
+// 3) this в конструкторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this: call, apply, bind
+
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', (e) => {
+    // this.style.height = '40px';
+    // this.style.length = '50px';
+    e.target.style.backgroundColor = 'green';
+});
+
+const obj = {
+    num: 5,
+    sayNumber: function(){
+        const say =() => {
+            console.log(this.num);
+        };
+        say();
+    }
 };
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
+obj.sayNumber();
 
-ivan.exit();
+const double = a => a * 2;
 
-ivan.hello();
-alex.hello();
-
-console.log(ivan);
-console.log(alex);
+console.log(double(4));
